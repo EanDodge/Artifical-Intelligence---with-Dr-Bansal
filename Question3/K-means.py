91cb38b5069be5e3fc205123445946176dc6ec89
EanDodge <145071390+EanDodge@users.noreply.github.com>
2024-11-05 19:04:11 -0500

    update

--- Question3/K-means.py
+++ Question3/K-means.py
@@ -8,6 +8,14 @@ def get_data():
         result.append((rand1,rand2))
     return result
 
+def points_in_marcoblocks(data, low_x, low_y, high_x, high_y):
+    num_of_macro = 0
+    for point in data:
+        if ((point[0] >= low_x) and (point[0]< high_x) 
+            and (point[1] >= low_y) and (point[1] < high_y )):
+            num_of_macro += 1
+    return num_of_macro
+
 def generate_seed(data, number_of_centriods):
     set_x = [data[0] for i in data]
     set_y = [data[1] for i in data]
@@ -22,13 +30,13 @@ def generate_seed(data, number_of_centriods):
         low_x = min_x + (i*size_x)
         high_x = low_x + size_x
         mid_x = (low_x + high_x)/2
-        for j in range(number_of_centriods):#y low = min_x + i × size_x; y high = y low + size_y;
+        for j in range(number_of_centriods):
             low_y = min_x + (i*size_x)
             high_y = low_y + size_y
             mid_y = (low_y + high_y)/2
-            #num_of_points = points_in_marcoblocks(data, low_x, low_y, high_x, high_y)
-            #if (num_of_points > avr_density):
-            #high_density = high_density + (mid_x , mid_y)
+            num_of_points = points_in_marcoblocks(data, low_x, low_y, high_x, high_y)
+            if (num_of_points > avr_density):
+                high_density = high_density + (mid_x , mid_y)
 
 
 
